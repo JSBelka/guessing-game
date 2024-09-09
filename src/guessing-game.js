bb34fa1b7ba4f16eb45257d0cb5f4969cf07e8c9
JSBelka <barbi-bar@mail.ru>
2024-09-09 16:21:47 +0300

    init: guessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.minimum = null;
+        this.maximum = null;
+    }
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
-
     guess() {
-
+        return Math.floor((this.minimum + this.maximum) / 2);
     }
-
     lower() {
-
+        this.maximum = this.guess() - 1;
     }
-
     greater() {
-
+        this.minimum = this.guess() + 1;
     }
 }
 
